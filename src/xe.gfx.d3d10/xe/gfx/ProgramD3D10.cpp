a720ab48b32baddf9dc95592148aa66bd0660e08
Felipe Apablaza <ing.apablaza@gmail.com>
2016-12-27 22:51:30 -0300

    Added initial D3D Files

---
+++ src/xe.gfx.d3d10/xe/gfx/ProgramD3D10.cpp
@@ -0,0 +1,19 @@
+
+#include "ProgramD3D10.hpp"
+
+namespace xe { namespace gfx {
+
+    ProgramD3D10::ProgramD3D10(const std::list<ShaderSource> &sources) {
+    }
+
+    ProgramD3D10::~ProgramD3D10() {
+    }
+
+    int ProgramD3D10::getUniform(const std::string &name) const {
+
+    }
+
+    int ProgramD3D10::getAttrib(const std::string &name) const {
+
+    }
+}}
