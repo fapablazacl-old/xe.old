1bd565a960028c3cc94014f2204d8747bc8e1c37
Felipe Apablaza <ing.apablaza@gmail.com>
2017-02-14 23:50:08 -0300

    Added missing files

---
+++ src/demo/Archive.cpp
@@ -0,0 +1,36 @@
+
+#include "Archive.hpp"
+
+#include <xe/FileStream.hpp>
+
+namespace xe {
+
+    /**
+     * @brief Simple stream instancer for files in a Directory.
+     */
+    class DirectoryArchive : public Archive {
+    public:
+        explicit DirectoryArchive(const std::string &base = "") : m_base(base) {
+            auto isSeparator = [](const char ch) {
+                return ch=='/' || ch=='\\';
+            };
+
+            if (isSeparator(base[base.size() - 1])) {
+                m_base += "/";
+            }
+        }
+
+        virtual ~DirectoryArchive() {}
+
+        virtual std::unique_ptr<Stream> open(const std::string& name) override {
+            return std::make_unique<FileStream>(m_base + name);
+        }
+
+    private:
+        std::string m_base;
+    };
+
+    std::unique_ptr<Archive> Archive::create(const std::string &source) {
+        return std::make_unique<DirectoryArchive>(source);
+    }
+}
---
+++ src/demo/Archive.hpp
@@ -0,0 +1,39 @@
+
+#pragma once
+
+#ifndef __xe_resourcemanager_hpp__
+#define __xe_resourcemanager_hpp__
+
+#include <memory>
+#include <string>
+#include <set>
+#include <xe/Stream.hpp>
+
+namespace xe {
+    class Stream;
+    
+    /**
+     * @brief Opens various streams from a common source.
+     *
+     * By source, we mean a raw directory, a zipped folder, a base URL, etc.
+     */
+    class Archive {
+    public:
+        virtual ~Archive() {}
+
+        /**
+         * @brief Open a stream from the specified source (syntax depends on the archive), in read-only mode.
+         */
+        virtual std::unique_ptr<Stream> open(const std::string& name) = 0;
+
+    public:
+        /**
+         * @brief Create a new Archive from the specified source.
+         *
+         * The only type of source supported for now is a physical directory.
+         */
+        static std::unique_ptr<Archive> create(const std::string &source);
+    };
+}
+
+#endif
